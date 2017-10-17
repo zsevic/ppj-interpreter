ba76fd0a0cb8620a1ce0e2018363c0ac94a4ece2
zsevic <zsevic95@gmail.com>
2017-10-18 01:54:29 +0200

    removed http part

--- MainListener.js
+++ MainListener.js
@@ -3,8 +3,8 @@ const Lexer = require('./VectorsLexer');
 const Parser = require('./VectorsParser');
 let VectorsListener = require('./VectorsListener').VectorsListener;
  
-MainListener = function(res) {
-    this.Res = res;    
+MainListener = function() {
+    this.table = {};
     VectorsListener.call(this);
     return this;
 };
@@ -12,14 +12,34 @@ MainListener = function(res) {
 MainListener.prototype = Object.create(VectorsListener.prototype);
 MainListener.prototype.constructor = MainListener;
  
-MainListener.prototype.enterDeclaration = function(ctx) {          
-    this.Res.write('<strong>');
+MainListener.prototype.enterStatement = function(ctx) {
+    if(ctx.PRINT()){
+        console.log(this.table[ctx.children[1].getText()]);
+    } 
 };
- 
-MainListener.prototype.exitDeclaration = function(ctx) {      
+
+MainListener.prototype.enterDeclaration = function(ctx) {
+    
+};
+
+MainListener.prototype.exitDeclaration = function(ctx) {
     let id = ctx.ID().getText();
-    console.log(id);
-    this.Res.write(`${id}</strong>`);
+    if(ctx.children.length===3){
+        let tmp = JSON.parse(ctx.children[2].getText());
+        this.table[id] = tmp.slice();
+    }else{
+        this.table[id] = [0,0,0];
+    }
 }; 
+
+MainListener.prototype.exitAssignment = MainListener.prototype.exitDeclaration;
+
+MainListener.prototype.enterE = function(ctx) {
+    let tmp=[];
+    for(let i=1;i<=5;i+=2){
+        tmp.push(ctx.children[i].getText());
+    }
+    return tmp;
+};
 
 exports.MainListener = MainListener;
--- antlr.js
+++ antlr.js
@@ -1,28 +1,14 @@
-const http = require('http');
 const antlr4 = require('antlr4/index');
 const Lexer = require('./VectorsLexer');
 const Parser = require('./VectorsParser');
 const MainListener = require('./MainListener').MainListener;
  
-http.createServer((req, res) => {
-   
-   res.writeHead(200, {
-          'Content-Type': 'text/html',        
-      });
- 
-   res.write('<html><head><meta charset="UTF-8"/></head><body>');
-   
-   var input = "vector p=[1,2,3];";
-   var chars = new antlr4.InputStream(input);
-   var lexer = new Lexer.VectorsLexer(chars);
-   var tokens  = new antlr4.CommonTokenStream(lexer);
-   var parser = new Parser.VectorsParser(tokens);
-   parser.buildParseTrees = true;   
-   var tree = parser.vectors();   
-   var vec = new MainListener(res);
-   antlr4.tree.ParseTreeWalker.DEFAULT.walk(vec, tree);
-   
-   res.write('</body></html>');
-   res.end();
- 
-}).listen(1337);
+let input = "vector p=[1,-2,3.2],p3;print p3;p3=[1,2,4];print p3;";
+let chars = new antlr4.InputStream(input);
+let lexer = new Lexer.VectorsLexer(chars);
+let tokens  = new antlr4.CommonTokenStream(lexer);
+let parser = new Parser.VectorsParser(tokens);
+parser.buildParseTrees = true;
+let tree = parser.vectors();
+let vec = new MainListener();
+antlr4.tree.ParseTreeWalker.DEFAULT.walk(vec, tree);
